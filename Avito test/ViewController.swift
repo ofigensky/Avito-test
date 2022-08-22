035819174fd13e8637b9b578e7203c872d17f7aa
ofigensky <bulletproofgleb@icloud.com>
2022-08-22 18:34:25 +0300

    Initial Commit

---
+++ Avito test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Avito test
+//
+//  Created by Gleb Glushok on 22.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
